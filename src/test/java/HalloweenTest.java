8cce891dc293e7e20cccce2eb1225f15b5f1e96b
RayEltaib <abdo.eltaib.1970@gmail.com>
2020-11-01 01:03:31 -0400

    update HalloweenTest.java

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,4 +1,7 @@
 public class HalloweenTest {
 
-    // the first time I try was the hardest.
+    // the first time I tried was the hardest.
+    public static void main(String[] args) {
+        System.out.println("First change");
+    }
 }
